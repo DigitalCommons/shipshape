69310d2e8f8944a9961533edc44fe5bd8e9a76e2
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-12-18 17:05:26 +0000

    strip away gatsby starter boilerplate, add our own

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,36 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import React, { useState, useEffect } from "react"
+import { withPrefix } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import axios from "axios"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
 
+const IndexPage = () => {
+  const [site, setSite] = useState({pending: true});
+  const apiUrl = withPrefix('/api/sea-map.php');
+  const siteUrl = 'https://dev.ica.solidarityeconomy.coop';
+  async function fetchSite(siteUrl) {
+    const url = `${apiUrl}?url=${siteUrl}`;
+    console.log("fetching", url);
+    try {
+      const response = await axios.get(url);
+      setSite(response.data)
+    } catch (e) {
+      setSite({error: e.message, stack: e.stack, url: url});
+    }
+  }
+  useEffect(() => {
+    // Update the document title using the browser API
+    fetchSite(siteUrl);
+  }, []);
+  return (
+    <Layout>
+      <h1>SEA Infrastructure</h1>
+      <pre>{ JSON.stringify(site, null, 2) }</pre>
+      <button onClick={() => fetchSite(siteUrl)}>
+        Refresh
+      </button>
+    </Layout>
+  )
+}
+  
 export default IndexPage
