392aece047fa350b533cb934060906405802191f
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-12-22 14:24:35 +0000

    php/api/sea-map.php - remove unused path and url data
    This script no longer needs to know about this data.

--- php/api/sea-map.php
+++ php/api/sea-map.php
@@ -7,53 +7,6 @@
  *
  */
 
-$paths = [
-    "coopsuk@dev-0" => "/var/www/vhosts/coopsuk.solidarityeconomy.coop/www",
-    "dotcoop-project@dev-0" => "/var/www/vhosts/dotcoop.solidarityeconomy.coop/www",
-    "ica-project@dev-0" => "/var/www/vhosts/ica.solidarityeconomy.coop/www",
-    "mutual-aid-project@dev-0" => "/var/www/vhosts/mutual-aid.solidarityeconomy.coop/www",
-    "newbridge-project@dev-0" => "/var/www/vhosts/newbridge.solidarityeconomy.coop/www",
-    "oxford-project@dev-0" => "/var/www/vhosts/oxford.solidarityeconomy.coop/www",
-
-    "coopsuk@prod-0" => "/var/www/vhosts/coopsuk.solidarityeconomy.coop/www",
-    "dotcoop-project@prod-0" => "/var/www/vhosts/dotcoop.solidarityeconomy.coop/www",
-    "ica-project@prod-0" => "/var/www/vhosts/ica.solidarityeconomy.coop/www",
-    "mutual-aid-project@prod-0" => "/var/www/vhosts/mutual-aid.solidarityeconomy.coop/www",
-    "newbridge-project@prod-0" => "/var/www/vhosts/newbridge.solidarityeconomy.coop/www",
-    "oxford-project@prod-0" => "/var/www/vhosts/oxford.solidarityeconomy.coop/www",
-    
-    "coopsuk@sea-0" => "/var/www/sea-maps/coopsuk-map",
-    "dotcoop-project@sea-0" => "/var/www/sea-maps/dotcoop-map",
-    "newbridge-project@sea-0" => "/var/www/sea-maps/newcastle-map",
-    "oxford-project@sea-0" => "/var/www/sea-maps/oxford-map",
-];
-
-$deployed = [
-    "dev-0" => [
-        "coopsuk" => "https://dev.coopsuk.solidarityeconomy.coop",
-        "dotcoop-project" => "https://dev.dotcoop.solidarityeconomy.coop",
-        "ica-project" => "https://dev.ica.solidarityeconomy.coop",
-        "mutual-aid-project" => "https://dev.mutual-aid.solidarityeconomy.coop",
-        "newbridge-project" => "https://dev.newbridge.solidarityeconomy.coop",
-        "oxford-project" => "https://dev.oxford.solidarityeconomy.coop",
-    ],
-    
-    "prod-0" => [
-        "coopsuk" => "https://prod.coopsuk.solidarityeconomy.coop",
-        "dotcoop-project" => "https://prod.dotcoop.solidarityeconomy.coop",
-        "ica-project" => "https://prod.ica.solidarityeconomy.coop",
-        "mutual-aid-project" => "https://prod.mutual-aid.solidarityeconomy.coop",
-        "newbridge-project" => "https://prod.newbridge.solidarityeconomy.coop",
-        "oxford-project" => "https://prod.oxford.solidarityeconomy.coop",
-    ],
-    "sea-0" => [
-        //        "coopsuk" => "https://coopsuk.solidarityeconomy.coop",
-        "dotcoop-project" => "https://dotcoop.solidarityeconomy.coop",
-        //"newbridge-project" => "https://newbridge.solidarityeconomy.coop",
-        //"oxford-project" => "https://oxford.solidarityeconomy.coop",
-    ],
-];
-
 
 function text($url) {
     $contents = file_get_contents($url);
