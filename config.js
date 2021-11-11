dd0e2ae3d0b2d1527958bdb9fef316d4db6b0ee6
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-11-11 11:54:48 +0000

    config.js - sort servers by name

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ module.exports = {
         "dotcoop-project": "https://dev.dotcoop.solidarityeconomy.coop",
         "ica-project": "https://dev.ica.solidarityeconomy.coop",
         "ica-youth-project": "https://dev.ica-youth-network.solidarityeconomy.coop",
+        "mersey-green-project": "https://dev.mersey-green.solidarityeconomy.coop",
         "mutual-aid-project": "https://dev.mutual-aid.solidarityeconomy.coop",
         "newbridge-project": "https://dev.newbridge.solidarityeconomy.coop",
         "oxford-project": "https://dev.oxford.solidarityeconomy.coop",
-        "mersey-green-project": "https://dev.mersey-green.solidarityeconomy.coop",
       },
       
       "prod": {
@@ -19,10 +19,10 @@ module.exports = {
         "dotcoop-project": "https://prod.dotcoop.solidarityeconomy.coop",
         "ica-project": "https://prod.ica.solidarityeconomy.coop",
         "ica-youth-project": "https://prod.ica-youth-network.solidarityeconomy.coop",
+        "mersey-green-project": "https://prod.mersey-green.solidarityeconomy.coop",
         "mutual-aid-project": "https://prod.mutual-aid.solidarityeconomy.coop",
         "newbridge-project": "https://prod.newbridge.solidarityeconomy.coop",
         "oxford-project": "https://prod.oxford.solidarityeconomy.coop",
-        "mersey-green-project": "https://prod.mersey-green.solidarityeconomy.coop",
       },
     },
   }
